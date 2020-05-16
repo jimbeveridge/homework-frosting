56990b414a5a537039a9b389806e8cc1e5430b9e
Jim Beveridge <jimb@connectedsw.com>
2020-05-15 19:24:06 -0700

    Fix sorting of the Points column. Use default sort column.
    Also:
    - Removed some stale code.
    - Create attributes in the rows for sorting instead of having
      DateTime be a magic suffix.
    
    Fixes #14
    Fixes #15

--- src/page_action/popup.js
+++ src/page_action/popup.js
@@ -69,15 +69,15 @@ document.getElementById("copyButton").addEventListener("click", _ => {
     copyFormatted(forToday.innerHTML);
 });
 
-function generateTableHead(table, data) {
+function generateTableHead(table, data, primary) {
     let thead = table.createTHead();
     let row = thead.insertRow();
     for (let key of data) {
         let th = document.createElement("th");
         th.className = key;
 
-        if (key == "points") {
-            th.setAttribute("data-sort-method", "number");
+        if (key == primary) {
+            th.setAttribute("data-sort-default", "");
         }
 
         let text = document.createTextNode(key);
@@ -86,108 +86,62 @@ function generateTableHead(table, data) {
     }
 }
 
-function CreateTableFromJSON(doc, myBooks, col, id) {
+function createTableFromJSON(doc, rows, headings, primary, id) {
 
-    // EXTRACT VALUE FOR HTML HEADER.
-    // ('Book ID', 'Book Name', 'Category' and 'Price')
-    if (col == null || col.length == 0) {
-        col = [];
-        for (var i = 0; i < myBooks.length; i++) {
-            for (var key in myBooks[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
+    // Determine the names of the columns
+    if (headings == null || headings.length == 0) {
+        headings = [];
+        for (var i = 0; i < rows.length; i++) {
+            for (var key in rows[i]) {
+                if (headings.indexOf(key) === -1) {
+                    headings.push(key);
                 }
             }
         }
     }
 
-    // CREATE DYNAMIC TABLE.
     var table = doc.createElement("table");
 
-    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-
-    // ADD JSON DATA TO THE TABLE AS ROWS.
-    for (var i = 0; i < myBooks.length; i++) {
+    for (var i = 0; i < rows.length; i++) {
 
         tr = table.insertRow();
 
-        for (let j = 0; j < col.length; j++) {
-            const key = col[j];
+        for (let j = 0; j < headings.length; j++) {
+            const heading = headings[j];
             let tabCell = tr.insertCell(-1);
-            tabCell.className = key;
-            if (key in myBooks[i]) {
-                data = myBooks[i][key];
+            tabCell.className = heading;
+
+            if (heading in rows[i]) {
+                data = rows[i][heading];
                 if (data === false) data = "-";
                 if (data === true) date = "<b>&#x2713;</b>";
 
                 tabCell.innerHTML = data;
 
-                const iso = key + "DateTime";
-                if (iso in myBooks[i]) {
-                    tabCell.setAttribute("data-sort", myBooks[i][iso]);
+                const sortHeading = heading + "Sort";
+                if (sortHeading in rows[i]) {
+                    tabCell.setAttribute("data-sort", rows[i][sortHeading]);
                 }
             }
         }
     }
 
     // Must do this last to get a tbody section
-    generateTableHead(table, col);
+    generateTableHead(table, headings, primary);
 
     // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
     var divContainer = doc.getElementById(id);
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
 
-    new Tablesort(table);
-}
-
-// Reverse sort
-function compareSubmittedDateTime(a, b) {
-    let bandA = "";
-    let bandB = "";
-
-    if ("submittedDateTime" in a) bandA = a.submittedDateTime;
-    if ("submittedDateTime" in b) bandB = b.submittedDateTime;
-
-    let comparison = 0;
-    if (bandA < bandB) {
-        comparison = 1;
-    } else if (bandA > bandB) {
-        comparison = -1;
+    let desc = {};
+    if (primary == "submitted") {
+        desc = {
+            descending: true
+        };
     }
-    return comparison;
-}
-
-function compareDueDateTime(a, b) {
-    let bandA = "";
-    let bandB = "";
-
-    if ("dueDateTime" in a) bandA = a.dueDateTime;
-    if ("dueDateTime" in b) bandB = b.dueDateTime;
-
-    let comparison = 0;
-    if (bandA > bandB) {
-        comparison = 1;
-    } else if (bandA < bandB) {
-        comparison = -1;
-    }
-    return comparison;
-}
-
-function compareClassName(a, b) {
-    let bandA = "";
-    let bandB = "";
-
-    if ("classname" in a) bandA = a.classname;
-    if ("classname" in b) bandB = b.classname;
-
-    let comparison = 0;
-    if (bandA > bandB) {
-        comparison = 1;
-    } else if (bandA < bandB) {
-        comparison = -1;
-    }
-    return comparison;
+    let ts = new Tablesort(table, desc);
+    ts.refresh();
 }
 
 // dt1 and dt2 are both iso date strings
@@ -199,18 +153,10 @@ function hoursDiff(dt1, dt2) {
     return diffTime / (1000 * 3600);
 }
 
-function daysDiff(dt1, dt2) {
-    // calculate the time difference of two dates JavaScript
-    let diffTime = (dt2.getTime() - dt1.getTime());
-    // calculate the number of days between two dates javascript
-    let daysDiff = diffTime / (1000 * 3600 * 24);
-    return daysDiff;
-}
-
 // If any of the words in `filters` appears in item.name,
 // then this is skipped.
 function is_skipped(name, filters) {
-    for (let i=0; i<filters.length; i++) {
+    for (let i = 0; i < filters.length; i++) {
         if (filters[i] == "") continue;
         if (name.toLowerCase().indexOf(filters[i].toLowerCase()) != -1) {
             return true;
@@ -236,6 +182,11 @@ function render(rows, updated, filter) {
         el.late = el.dueDateTime < nowIso && (!el.completed || el.missingSubmission);
         el.due = dateFormat(new Date(el.dueDateTime), "ddd, mmm d, h:MM TT")
         el.submitted = el.submittedDateTime != "" ? dateFormat(new Date(el.submittedDateTime), "ddd, mmm d, h:MM TT") : "";
+
+        el.dueSort = el.dueDateTime;
+        el.submittedSort = el.submittedDateTime;
+        el.pointsSort = el.points > 0 ? ("" + el.points).padStart(5, "0") : "00000";
+
         if ("rework" in el) {
             if (el.rework) {
                 el.today = true;
@@ -243,6 +194,8 @@ function render(rows, updated, filter) {
                 el.completed = false;
             }
         }
+
+
         if (el.completed && hoursDiff(el.submittedDateTime, updated) < 24) {
             el.name += " &#11088;";
         }
@@ -251,11 +204,9 @@ function render(rows, updated, filter) {
     let el = document.getElementById("updated");
     el.innerText = dateFormat(new Date(updated), "ddd, mmm dS, yyyy, h:MM TT");
 
-    rows = rows.sort(compareDueDateTime);
-
     // TODO Issue #
     let filters = filter.trim().split("\n");
-    for (let i=0; i<filters.length; i++) {
+    for (let i = 0; i < filters.length; i++) {
         if (filters[i].trim() == "")
             filters[i] = "";
     }
@@ -264,17 +215,17 @@ function render(rows, updated, filter) {
     let headings = ["classname", "name", "due", "points"];
 
     var wanted = rows.filter((item) => item.today && !item.completed && !item.late);
-    CreateTableFromJSON(document, wanted, headings, "forToday");
+    createTableFromJSON(document, wanted, headings, "due", "forToday");
 
     wanted = rows.filter((item) => item.late);
-    CreateTableFromJSON(document, wanted, headings, "forLate");
+    createTableFromJSON(document, wanted, headings, "due", "forLate");
 
     wanted = rows.filter((item) => !item.completed && !item.late && !item.today);
-    CreateTableFromJSON(document, wanted, headings, "forUpcoming");
+    createTableFromJSON(document, wanted, headings, "due", "forUpcoming");
 
     headings = ["classname", "name", "submitted", "points"];
-    wanted = rows.filter((item) => item.completed && !item.late).sort(compareSubmittedDateTime);
-    CreateTableFromJSON(document, wanted, headings, "forCompleted");
+    wanted = rows.filter((item) => item.completed && !item.late);
+    createTableFromJSON(document, wanted, headings, "submitted", "forCompleted");
 }
 
 async function render_table_from_storage() {
